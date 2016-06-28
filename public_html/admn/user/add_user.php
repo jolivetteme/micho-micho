20dc0ed7fa51cf40deeb983afd0781adaa922596
Mitchell Jolivette <jolivetteme@shoyanet.com>
2016-06-27 19:28:10 -0500

    Begin Add New Users

--- public_html/admn/user/add_user.php
+++ public_html/admn/user/add_user.php
@@ -37,7 +37,7 @@ desired effect
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Our Employees
+        Add New Members/ Employees
         <small>What are they up to?</small>
       </h1>
       <ol class="breadcrumb">
@@ -50,8 +50,28 @@ desired effect
     <!-- Main content -->
     <section class="content">
       <!-- Your Page Content Here -->
+        <div class="register-box-body">
+          <p class="login-box-msg">Add New User</p>
 
-        
+          <form action="../../index.html" method="post">
+            <div class="form-group has-feedback">
+              <input type="text" class="form-control" placeholder="Full name">
+              <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <input type="email" class="form-control" placeholder="Email">
+              <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <input type="password" class="form-control" placeholder="Password">
+              <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <input type="password" class="form-control" placeholder="Retype password">
+              <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+            </div>
+          </form>
+        </div>
 
     </section>
     <!-- /.content -->
